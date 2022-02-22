ba440f02d4731fb132cfbfaa2e65a27e7a0c2045
Soham Shinde <sohamshinde04@gmail.com>
2022-02-22 14:36:48 +0530

    added alert and vairables

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 let r = "hi"
+
+function gen() {
+    alert("hi")
+
+    let title = document.getElementById('title').value
+    let stipend = document.getElementById('stipend').value
+    let time = document.getElementById('time').value
+    let position = document.getElementById('position').value
+    let number_of_positions = document.getElementById('number-of-positions').value
+    let form_link = document.getElementById('form-link').value
+    let deadline = document.getElementById('deadline').value
+
+    
+
+}
